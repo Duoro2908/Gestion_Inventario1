7605692c482c6c1bd5dba30b881d2597e41bfa47
Duoro2908 <elvinalexandermendezcruz05@gmail.com>
2024-11-08 18:54:59 -0600

    primer cambio confirmado

---
+++ Gestion_Inventario/Gestion_Inventario/Inventario.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Inventario
+{
+    public class Inventario
+    {
+        private List<Producto> productos;
+
+        public Inventario()
+        {
+            productos = new List<Producto>();
+        }
+        public void Agregar_Producto(Producto producto)
+        {
+            productos.Add(producto);
+        }
+        public IEnumerable<Producto> Filtrar_Y_Ordenar_Productos(double precio_Minimo)
+        {
+            return productos
+                .Where(p => p.Precio > precio_Minimo)
+                .OrderBy(p => p.Precio);
+        }
+
+        public void Precio_Actualizado(string nombre_producto, double Precio_nuevo)
+        {
+            var producto = productos.FirstOrDefault(p => p.Nombre == nombre_producto);
+            if (producto != null)
+            {
+                producto.Precio = Precio_nuevo;
+                Console.WriteLine($"El precio de {nombre_producto} ha sido actualizado a {Precio_nuevo}");
+            }
+            else
+            {
+                Console.WriteLine("Lo sentimos producto no encontrado");
+            }
+        }
+        public void Productos_Eliminados(string nombre_producto)
+        {
+            var producto = productos.FirstOrDefault(p => p.Nombre == nombre_producto);
+            if (producto != null)
+            {
+                productos.Remove(producto);
+                Console.WriteLine($"El producto {nombre_producto} fue eliminado");
+            }
+            else
+            {
+                Console.WriteLine("EL producto no fue encontrado");
+            }
+        }
+
+
+        public void Contar_Productos_Por_Rango_De_Precio()
+        {
+            int rango_Menor_150 = productos.Count(p => p.Precio < 150);
+            int rango_Entre_150y500 = productos.Count(p => p.Precio >= 150 && p.Precio <= 500);
+            int rango_Mayor_500 = productos.Count(p => p.Precio > 500);
+
+
+            Console.WriteLine("\n");
+            Console.WriteLine($"Productos que tienen precio menor a 150: {rango_Menor_150}");
+            Console.WriteLine($"Productos que tienen precio entre 150 y 500: {rango_Entre_150y500}");
+            Console.WriteLine($"Productos que tienen precio mayor a 500: {rango_Mayor_500}");
+        }
+
+
+        public void Reportes_Resumidos()
+        {
+            if (productos.Count == 0)
+            {
+                Console.WriteLine("No hay ningun producto en el inventario.");
+                return;
+            }
+
+            double precio_Promedio = productos.Average(p => p.Precio);
+            var producto_Mas_Caro = productos.OrderByDescending(p => p.Precio).First();
+            var producto_Mas_Barato = productos.OrderBy(p => p.Precio).First();
+
+            Console.WriteLine("\n");
+            Console.WriteLine($"Aqui le proporcionamos un Reporte de inventario");
+            Console.WriteLine($"Total de productos: {productos.Count}");
+            Console.WriteLine($"Precio promedio: {precio_Promedio}");
+            Console.WriteLine($"Producto más caro: {producto_Mas_Caro.Nombre} con precio de: {producto_Mas_Caro.Precio}");
+            Console.WriteLine($"Producto más barato: {producto_Mas_Barato.Nombre}  con precio de: {producto_Mas_Barato.Precio}");
+        }
+    }
+}
+
+
+
+
+
---
+++ Gestion_Inventario/Gestion_Inventario/Sistema_Producto.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Gestion_Inventario
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Inventario miinventario = new Inventario();
+            Console.WriteLine("Bienvenido al sistema de Gestión de Inventario");
+
+
+            Console.Write("Cuantos productos desea ingresar? ");
+            int cantidad = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.WriteLine($"Producto {i + 1}:");
+
+                Console.Write("Nombre: ");
+                string nombre = Console.ReadLine();
+                while (nombre == "")
+                {
+                    Console.WriteLine("El nombre no puede estar vacío.");
+                    Console.Write("Nombre: ");
+                    nombre = Console.ReadLine();
+                }
+
+                double precio;
+                do
+                {
+                    Console.Write("Precio: ");
+                    precio = double.Parse(Console.ReadLine());
+                    if (precio <= 0)
+                    {
+                        Console.WriteLine("El precio debe ser un número positivo corrija");
+                    }
+                } while (precio <= 0);
+
+                Producto miproducto = new Producto(nombre, precio);
+                miinventario.Agregar_Producto(miproducto);
+                Console.WriteLine("\n");
+            }
+
+            while (true)
+            {
+                Console.WriteLine("puedes agregar alguna opcion si deseas:");
+                Console.WriteLine("\n");
+                Console.WriteLine("1 Filtrar y ordenar productos por precio mínimo");
+                Console.WriteLine("2 Actualizar precio de un producto");
+                Console.WriteLine("3 Eliminar un producto");
+                Console.WriteLine("4 Contar y agrupar productos por rango de precio");
+                Console.WriteLine("5 Generar reporte del inventario");
+                Console.WriteLine("6. Salir");
+
+                Console.Write("Seleccione una opción por favor: ");
+                int opc = int.Parse(Console.ReadLine());
+
+                switch (opc)
+                {
+                    case 1:
+                        Console.Write("Ingrese el precio mínimo para filtrar los productos: ");
+                        double precio_Minimo = double.Parse(Console.ReadLine());
+                        var productos_Filtrados = miinventario.Filtrar_Y_Ordenar_Productos(precio_Minimo);
+                        Console.WriteLine("\n");
+
+                        Console.WriteLine("Productos filtrados y ordenados:");
+                        foreach (var producto in productos_Filtrados)
+                        {
+                            producto.Mostrar_Informacion();
+                        }
+                        break;
+
+                    case 2:
+                        Console.Write("Ingrese el nombre del producto a actualizar: ");
+                        string nombre_A_actualizar = Console.ReadLine();
+                        Console.WriteLine("\n");
+                        Console.Write("Ingrese el precio nuevo: ");
+                        double Precio_nuevo = double.Parse(Console.ReadLine());
+                        miinventario.Precio_Actualizado(nombre_A_actualizar, Precio_nuevo);
+                        break;
+
+                    case 3:
+                        Console.Write("Ingrese el nombre del producto que quiere eliminar: ");
+                        string nombre_Eliminar = Console.ReadLine();
+                        Console.WriteLine("\n");
+                        miinventario.Productos_Eliminados(nombre_Eliminar);
+                        break;
+
+                    case 4:
+                        miinventario.Contar_Productos_Por_Rango_De_Precio();
+
+                        break;
+
+                    case 5:
+                        miinventario.Reportes_Resumidos();
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Gracias por utilizar el sistema fue un gusto servirle");
+                        Console.WriteLine("Att: Futuro Ing: Alexander Mendez");
+                        return;
+
+                    default:
+                        Console.WriteLine("Opción no válida, intente nuevamente");
+                        break;
+                }
+            }
+        }
+    }
+}
